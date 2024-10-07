e6d8e3f8a693297f739d2a1bab726f236efb9d4d
Marcel <8513034+MrEAlderson@users.noreply.github.com>
2024-10-07 14:21:13 +0200

    Depend on 5.4.14

--- src/main/java/de/marcely/bedwars/levelshop/LevelShopPlugin.java
+++ src/main/java/de/marcely/bedwars/levelshop/LevelShopPlugin.java
@@ -6,9 +6,33 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class LevelShopPlugin extends JavaPlugin {
 
+  private static final byte MBEDWARS_API_NUM = 113;
+  private static final String MBEDWARS_API_NAME = "5.4.14";
+
   @Override
   public void onEnable() {
+    if (!validateMBedwars())
+      return;
+
     GameAPI.get().registerCustomSpawnerHandler(new LevelDropTypeHandler(this));
     Bukkit.getPluginManager().registerEvents(new LevelEventsHandler(), this);
+  }
+
+
+  private boolean validateMBedwars() {
+    try {
+      final Class<?> apiClass = Class.forName("de.marcely.bedwars.api.BedwarsAPI");
+      final int apiVersion = (int) apiClass.getMethod("getAPIVersion").invoke(null);
+
+      if (apiVersion < MBEDWARS_API_NUM)
+        throw new IllegalStateException();
+    } catch (Exception e) {
+      getLogger().warning("Sorry, your installed version of MBedwars is not supported. Please install at least v" + MBEDWARS_API_NAME);
+      Bukkit.getPluginManager().disablePlugin(this);
+
+      return false;
+    }
+
+    return true;
   }
 }
