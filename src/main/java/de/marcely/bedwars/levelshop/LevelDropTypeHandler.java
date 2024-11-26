1199c9e19097923e02f1387195fde23a0cb23b2b
Marcel <8513034+MrEAlderson@users.noreply.github.com>
2024-11-26 15:25:12 +0100

    Added configurable dropping level amount

--- src/main/java/de/marcely/bedwars/levelshop/LevelDropTypeHandler.java
+++ src/main/java/de/marcely/bedwars/levelshop/LevelDropTypeHandler.java
@@ -3,6 +3,7 @@ package de.marcely.bedwars.levelshop;
 import de.marcely.bedwars.api.game.spawner.CustomDropTypeHandler;
 import de.marcely.bedwars.api.game.spawner.Spawner;
 import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.entity.Player;
@@ -17,7 +18,16 @@ public class LevelDropTypeHandler extends CustomDropTypeHandler {
   public void handleDrop(Spawner spawner, Location dropLocation) {
     final ExperienceOrb orb = (ExperienceOrb) dropLocation.getWorld().spawnEntity(dropLocation, EntityType.EXPERIENCE_ORB);
 
-    orb.setExperience(1); // 1 exp = 1 level
+    orb.setExperience(getDropAmount(spawner)); // 1 exp = 1 level
+  }
+
+  private static int getDropAmount(Spawner spawner) {
+    final ConfigurationSection config = spawner.getDropType().getCustomHandlerConfig();
+
+    if (config == null)
+      return 1;
+
+    return Math.max(1, config.getInt("level-amount", 1));
   }
 
   @Override
