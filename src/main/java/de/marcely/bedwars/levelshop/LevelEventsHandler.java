6494cb6bb447e9feb7f382c882c631f65096c569
Marcel <8513034+MrEAlderson@users.noreply.github.com>
2024-11-26 16:13:32 +0100

    Fix breaking exp outside arenas, fix exp not giving properly sometimes

--- src/main/java/de/marcely/bedwars/levelshop/LevelEventsHandler.java
+++ src/main/java/de/marcely/bedwars/levelshop/LevelEventsHandler.java
@@ -1,20 +1,51 @@
 package de.marcely.bedwars.levelshop;
 
+import de.marcely.bedwars.api.GameAPI;
+import de.marcely.bedwars.api.arena.Arena;
+import de.marcely.bedwars.api.arena.ArenaStatus;
+import lombok.AllArgsConstructor;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 
-public class LevelEventsHandler implements Listener {
+@AllArgsConstructor
+class LevelEventsHandler implements Listener {
+
+  private final LevelShopPlugin plugin;
 
   @EventHandler
   public void onPlayerExpChangeEvent(PlayerExpChangeEvent event) {
-    if (event.getAmount() == 0)
+    if (event.getAmount() <= 0)
       return;
 
     final Player player = event.getPlayer();
+    final Arena arena = GameAPI.get().getArenaByPlayer(player);
+
+    if (arena == null || (arena.getStatus() != ArenaStatus.RUNNING && arena.getStatus() != ArenaStatus.END_LOBBY) ||
+        player.getGameMode() == GameMode.SPECTATOR)
+      return;
+
+    // Hotfix: level and total exp desynced
+    // Yes, they are internally two different variables that bukkit doesn't update properly
+    if (player.getExp() == 0 && player.getLevel() == 0 && player.getTotalExperience() != 0)
+      player.setTotalExperience(0);
+
     final int addExp = Util.getTotalExp(event.getAmount()+player.getLevel()) - player.getTotalExperience();
 
     event.setAmount(addExp);
+
+    // Hotfix: Exp rounding errors (vanilla problem?)
+    Bukkit.getScheduler().runTask(this.plugin, () -> {
+      if (arena.getPlayerTeam(player) == null || player.getGameMode() == GameMode.SPECTATOR)
+        return;
+
+      if (player.getExpToLevel() == 0 || player.getExp() >= 0.99F) {
+        player.setLevel(player.getLevel()+1);
+        player.setExp(0);
+      }
+    });
   }
 }
