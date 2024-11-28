8665730732f7da8610de46a06d6677ec570d048b
Marcel <8513034+MrEAlderson@users.noreply.github.com>
2024-11-28 13:11:46 +0100

    Added API when a player picks up an orb

--- src/main/java/de/marcely/bedwars/levelshop/LevelEventsHandler.java
+++ src/main/java/de/marcely/bedwars/levelshop/LevelEventsHandler.java
@@ -3,11 +3,13 @@ package de.marcely.bedwars.levelshop;
 import de.marcely.bedwars.api.GameAPI;
 import de.marcely.bedwars.api.arena.Arena;
 import de.marcely.bedwars.api.arena.ArenaStatus;
+import de.marcely.bedwars.levelshop.api.PlayerPickupOrbEvent;
 import lombok.AllArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 
@@ -16,7 +18,7 @@ class LevelEventsHandler implements Listener {
 
   private final LevelShopPlugin plugin;
 
-  @EventHandler
+  @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
   public void onPlayerExpChangeEvent(PlayerExpChangeEvent event) {
     if (event.getAmount() <= 0)
       return;
@@ -25,15 +27,28 @@ class LevelEventsHandler implements Listener {
     final Arena arena = GameAPI.get().getArenaByPlayer(player);
 
     if (arena == null || (arena.getStatus() != ArenaStatus.RUNNING && arena.getStatus() != ArenaStatus.END_LOBBY) ||
-        player.getGameMode() == GameMode.SPECTATOR)
+        player.getGameMode() == GameMode.SPECTATOR) {
       return;
+    }
+
+    // ask api
+    final PlayerPickupOrbEvent apiEvent = new PlayerPickupOrbEvent(player, arena, event.getAmount());
+
+    Bukkit.getPluginManager().callEvent(apiEvent);
+
+    final int level = apiEvent.getLevelAmount();
+
+    if (level == 0) {
+      event.setAmount(0);
+      return;
+    }
 
     // Hotfix: level and total exp desynced
     // Yes, they are internally two different variables that bukkit doesn't update properly
     if (player.getExp() == 0 && player.getLevel() == 0 && player.getTotalExperience() != 0)
       player.setTotalExperience(0);
 
-    final int addExp = Util.getTotalExp(event.getAmount()+player.getLevel()) - player.getTotalExperience();
+    final int addExp = Util.getTotalExp(level+player.getLevel()) - player.getTotalExperience();
 
     event.setAmount(addExp);
 
---
+++ src/main/java/de/marcely/bedwars/levelshop/api/PlayerPickupOrbEvent.java
@@ -0,0 +1,63 @@
+package de.marcely.bedwars.levelshop.api;
+
+import de.marcely.bedwars.api.arena.Arena;
+import de.marcely.bedwars.api.event.arena.ArenaEvent;
+import de.marcely.bedwars.tools.Validate;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+
+/**
+ * Gets called when a player picks up an orb and thereby earns level.
+ * <p>
+ *   It is not possible to cancel or obtain the orb entity, as the bukkit API
+ *   doesn't provide an easy solution for that either.
+ * </p>
+ */
+public class PlayerPickupOrbEvent extends PlayerEvent implements ArenaEvent {
+
+  private static final HandlerList HANDLERS = new HandlerList();
+
+  @Getter
+  private final Arena arena;
+
+  private int levelAmount;
+
+  public PlayerPickupOrbEvent(Player player, Arena arena, int levelAmount) {
+    super(player);
+
+    this.arena = arena;
+    this.levelAmount = levelAmount;
+  }
+
+  /**
+   * Get the amount of levels the player is about to earn.
+   *
+   * @return The amount of levels he picked up
+   */
+  public int getLevelAmount() {
+    return this.levelAmount;
+  }
+
+  /**
+   * Set the amount of levels the player is about to earn.
+   *
+   * @param levelAmount The amount of levels he picked up
+   * @throws IllegalArgumentException levelAmount must be 0 or greater
+   */
+  public void setLevelAmount(int levelAmount) {
+    Validate.isTrue(levelAmount >= 0, "Level amount must be greater or equal to 0");
+
+    this.levelAmount = levelAmount;
+  }
+
+  @Override
+  public HandlerList getHandlers() {
+    return HANDLERS;
+  }
+
+  public static HandlerList getHandlerList() {
+    return HANDLERS;
+  }
+}
